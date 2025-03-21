f9cbbfa0e06ee5f582d0f4e1836c11ae3495d01c
kK4ssio <diasmonteirokassio@gmail.com>
2025-03-21 16:55:41 -0300

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import logoImg from "./assets/logo.svg";
 import { Card } from "./components/card";
 import laCumaImg from "./assets/la-cuma.png"
+import LocalPng from "./assets/image.png"
+
 export function App() {
   return (
     <>
@@ -18,6 +20,43 @@ export function App() {
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
           price="R$29,90"
           />
+
+<Card 
+          imgUrl={laCumaImg}
+          name='La Cuma'
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          price="R$29,90"
+          />
+
+<Card 
+          imgUrl={laCumaImg}
+          name='La Cuma'
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          price="R$29,90"
+          />
+
+<Card 
+          imgUrl={laCumaImg}
+          name='La Cuma'
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          price="R$29,90"
+          />
+        </div>
+
+      </div>
+      <div className="about">
+        <img src={LocalPng} alt="" />
+
+        <div className="description">
+          <p className="d-title">Sobre a La P
+          <span>i</span>
+          <span>z</span>
+          <span>z</span>
+          a</p>
+
+          <p className="history">Bem-vindo ao "La Pizza", o seu refúgio acolhedor para uma experiência de pizzaria autêntica. Em nosso pequeno oásis italiano, cada pedaço de pizza é uma obra-prima artesanal. Sinta-se em casa enquanto o aroma sedutor de massa fresca assa no forno, criando as pizzas mais deliciosas da cidade.</p>
+
+          <button type="button">Ler mais sobre</button>
         </div>
       </div>
     </>
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,15 +1,23 @@
 import "./styles.css";
+import {Heart} from 'lucide-react';
+
 
 export function Card({ imgUrl, name, description, price }) {
   return (
     <div className="card">
+     
       <img src={imgUrl} alt={name} className="product-img" />
+      <div className="fav-btn">
+        <Heart size={18} color='#ff3536' fill="#ff3536" className="fav-icon"/>
+      </div>
+      
+
       <div className="products-details">
         <h1>{name}</h1>
         <p>{description}</p>
         <strong>{price}</strong>
       </div>
-      <button type="button">Comprar agora</button>
+      <button className="checkout-btn" type="button">Comprar agora</button>
     </div>
   );
 }
